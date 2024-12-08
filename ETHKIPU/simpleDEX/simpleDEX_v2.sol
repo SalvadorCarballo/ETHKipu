042b498e7d1dd0dea1c1eaf19dbb53940b793e3d
SalvadorCarballo <salvador.carballo@gmail.com>
2024-12-08 14:24:20 -0300

    Se modificó el formato de comentarios según el modelo que enviaron (Mario A. Parodi)

--- ETHKIPU/simpleDEX/simpleDEX_v2.sol
+++ ETHKIPU/simpleDEX/simpleDEX_v2.sol
@@ -4,106 +4,147 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-/* As suggested in the feedback, the Ownable library was added. 
-When doing so, the compiler gave an error requesting that a parameter with the address of the initial owner of the contract be added to the constructor. 
-It seems that the version of OpenZeppelin that I have in Remix does not automatically assign the person who deploys the contract as the initial owner. */
+/// @title Simple Decentralized Exchange (DEX)
+/// @notice This contract facilitates token swaps and liquidity management
+/// @notice The comment format was updated according to the model that was indicated
+/// @notice As suggested in the feedback, the Ownable library was added. When doing so, the compiler gave an error requesting that a parameter with the address of the initial owner of the contract be added to the constructor. It seems that the version of OpenZeppelin that I have in Remix does not automatically assign the person who deploys the contract as the initial owner
+/// @dev Uses the constant product formula for swaps
+/// @author Salvador Carballo
 
 contract SimpleDEX is Ownable {
-    IERC20 public tokenA;  // ERC-20 token contracts that can be traded on the simpleDEX
-    IERC20 public tokenB;  // ERC-20 token contracts that can be traded on the simpleDEX
-    uint256 public balanceofA; // Stores the amount of tokenA in the liquidity pool
-    uint256 public balanceofB; // Stores the amount of tokenB in the liquidity pool
+    /// @notice ERC-20 token A in the liquidity pool
+    IERC20 public tokenA; 
 
-    event LiquidityAdded(uint256 amountA, uint256 amountB); // "Liquidity Increase" event
-    event TokensSwappedAB(address indexed user, uint256 amountAIn, uint256 amountBOut); // "Swap of tokens A for B" event
-    event TokensSwappedBA(address indexed user, uint256 amountBIn, uint256 amountAOut); // "Swap of tokens B for A" event
-    event LiquidityRemoved(uint256 amountA, uint256 amountB); // "Decreased Liquidity" event
+    /// @notice ERC-20 token B in the liquidity pool
+    IERC20 public tokenB;
+    
+    /// @notice Stores the amount of token A in the liquidity pool
+    uint256 public balanceofA; 
+    
+    /// @notice Stores the amount of token B in the liquidity pool
+    uint256 public balanceofB;
 
-    // The token1 contract address, token2 contract address, and the initial owner are passed as parameters to the Ownable constructor.
+    /// @notice Emitted when liquidity is added
+    /// @param amountA The amount of token A added to the pool
+    /// @param amountB The amount of token B added to the pool
+    event LiquidityAdded(uint256 amountA, uint256 amountB); 
+
+    /// @notice Emitted when token A is swapped for token B
+    /// @param user The address of the user performing the swap
+    /// @param amountAIn The amount of token A swapped
+    /// @param amountBOut The amount of token B received
+    event TokensSwappedAB(address indexed user, uint256 amountAIn, uint256 amountBOut); 
+    
+    /// @notice Emitted when token B is swapped for token A
+    /// @param user The address of the user performing the swap
+    /// @param amountBIn The amount of token B swapped
+    /// @param amountAOut The amount of token A received
+    event TokensSwappedBA(address indexed user, uint256 amountBIn, uint256 amountAOut); 
+    
+    /// @notice Emitted when liquidity is removed
+    /// @param amountA The amount of token A removed from the pool
+    /// @param amountB The amount of token B removed from the pool
+    event LiquidityRemoved(uint256 amountA, uint256 amountB);
+
+
+    /// @notice Initializes the DEX with the two tokens and sets the initial owner
+    /// @param _tokenA The address of token A
+    /// @param _tokenB The address of token B
+    /// @param initialOwner The address of the initial owner
     constructor(address _tokenA, address _tokenB, address initialOwner) Ownable(initialOwner) {
         tokenA = IERC20(_tokenA);
         tokenB = IERC20(_tokenB);
     }
 
-    // Modifier to check if there is enough liquidity
+    /// @notice Modifier to ensure sufficient liquidity in the pool
     modifier EnoughLiquidity() {
         require(balanceofA > 0 && balanceofB > 0, "Not enough liquidity"); 
         _;
     }
 
-    // Add liquidity to the pool by depositing tokenA and tokenB amounts
+    /// @notice Adds liquidity to the pool
+    /// @dev Only callable by the owner
+    /// @param amountA The amount of token A to add
+    /// @param amountB The amount of token B to add
     function addLiquidity(uint256 amountA, uint256 amountB) external onlyOwner {
-        require(amountA > 0 && amountB > 0, "Amounts must be greater than 0"); //I request that the amounts of tokenA and tokenB be greater than 0
+        require(amountA > 0 && amountB > 0, "Amounts must be greater than 0"); 
 
-        tokenA.transferFrom(msg.sender, address(this), amountA); //transfer amountA of tokenA from the owner to the contract
-        tokenB.transferFrom(msg.sender, address(this), amountB); //transfer amountB of tokenB from the owner to the contract
+        tokenA.transferFrom(msg.sender, address(this), amountA);
+        tokenB.transferFrom(msg.sender, address(this), amountB);
 
-        balanceofA += amountA; //Update the balance in the tokenA pool
-        balanceofB += amountB; //Update the balance in the tokenB pool
+        balanceofA += amountA;
+        balanceofB += amountB;
 
-        emit LiquidityAdded(amountA, amountB); //generates "Liquidity Increase" event
+        emit LiquidityAdded(amountA, amountB);
     }
 
-    // Swap tokenA for tokenB
+    /// @notice Swaps token A for token B
+    /// @param amountAIn The amount of token A to swap
     function swapAforB(uint256 amountAIn) external EnoughLiquidity {
-        require(amountAIn > 0, "Amount must be greater than 0");//I request that tokenA amounts be greater than 0 for the swap
+        require(amountAIn > 0, "Amount must be greater than 0");
 
-        //To calculate the amount of tokens that a user will receive when making the swap, according to (x+dx)*(y-dy) = x*y => dy = y-(x*y)/(x+dx)
-        uint256 denominator = balanceofA + amountAIn; // (x+dx)
-        uint256 fraction = (balanceofA * balanceofB) / denominator; // (x*y)/(x+dx)
-        uint256 amountBOut = balanceofB - fraction; // dy=y-((x*y)/(x+dx))
+        /// @notice To calculate the amount of tokens that a user will receive when making the swap, according to (x+dx)*(y-dy) = x*y => dy = y-(x*y)/(x+dx)
+        uint256 denominator = balanceofA + amountAIn; 
+        uint256 fraction = (balanceofA * balanceofB) / denominator; 
+        uint256 amountBOut = balanceofB - fraction; 
 
-        require(amountBOut > 0 && amountBOut <= balanceofB, "Invalid output amount");//Check that "amountBout" is not negative and is less than the total balance of tokenB
+        require(amountBOut > 0 && amountBOut <= balanceofB, "Invalid output amount");
 
-        tokenA.transferFrom(msg.sender, address(this), amountAIn); //Transfer the amount "amountAIn" of tokenA from the user to the contract
-        tokenB.transfer(msg.sender, amountBOut); //Send the amount "amountBOut" of tokenB to the user
+        tokenA.transferFrom(msg.sender, address(this), amountAIn);
+        tokenB.transfer(msg.sender, amountBOut);
 
-        balanceofA += amountAIn; //Update the balance in the tokenA pool
-        balanceofB -= amountBOut; //Update the balance in the tokenB pool
+        balanceofA += amountAIn;
+        balanceofB -= amountBOut;
 
-        emit TokensSwappedAB(msg.sender, amountAIn, amountBOut); //Generates the event "Swap of tokens A for B"
+        emit TokensSwappedAB(msg.sender, amountAIn, amountBOut);
     }
 
-    //To swap tokenB for tokenA
+    /// @notice Swaps token B for token A
+    /// @param amountBIn The amount of token B to swap
     function swapBforA(uint256 amountBIn) external EnoughLiquidity {
-        require(amountBIn > 0, "Amount must be greater than 0");//I request that tokenB amounts be greater than 0 for the swap
+        require(amountBIn > 0, "Amount must be greater than 0");
 
-        //To calculate the amount of tokens that a user will receive when making the swap, according to (x+dx)*(y-dy) = x*y => dX = (x*y)/(y-dy)-x
-        uint256 denominator = balanceofB + amountBIn; // (y-dy)
-        uint256 fraction = (balanceofA * balanceofB) / denominator; // (x*y)/(y-dy)
-        uint256 amountAOut = balanceofA - fraction; // dx=((x*y)/(y-dy))-x
+        /// @notice To calculate the amount of tokens that a user will receive when making the swap, according to (x+dx)*(y-dy) = x*y => dX = (x*y)/(y-dy)-x
+        uint256 denominator = balanceofB + amountBIn;
+        uint256 fraction = (balanceofA * balanceofB) / denominator;
+        uint256 amountAOut = balanceofA - fraction;
 
-        require(amountAOut > 0 && amountAOut <= balanceofA, "Invalid output amount"); //Check that "amountAout" is not negative and is less than the total balance of tokenA
+        require(amountAOut > 0 && amountAOut <= balanceofA, "Invalid output amount"); 
 
-        tokenB.transferFrom(msg.sender, address(this), amountBIn); //Transfer the amount "amountBIn" of tokenB from the user to the contract
-        tokenA.transfer(msg.sender, amountAOut); //Send the amount "amountAOut" of tokenB to the user
+        tokenB.transferFrom(msg.sender, address(this), amountBIn); 
+        tokenA.transfer(msg.sender, amountAOut);
 
-        balanceofB += amountBIn; //Update the balance in the tokenB pool 
-        balanceofA -= amountAOut; //Update the balance in the tokenA pool
+        balanceofB += amountBIn;
+        balanceofA -= amountAOut;
 
-        emit TokensSwappedBA(msg.sender, amountBIn, amountAOut); //Generates the event "Swap of tokens B for A"
+        emit TokensSwappedBA(msg.sender, amountBIn, amountAOut);
     }
 
-    //For the owner to withdraw liquidity
+    /// @notice Removes liquidity from the pool
+    /// @dev Only callable by the owner
+    /// @param amountA The amount of token A to remove
+    /// @param amountB The amount of token B to remove
     function removeLiquidity(uint256 amountA, uint256 amountB) external onlyOwner EnoughLiquidity {
-        require(amountA <= balanceofA && amountB <= balanceofB, "Not enough liquidity"); //I check that the requested quantities are available
+        require(amountA <= balanceofA && amountB <= balanceofB, "Not enough liquidity"); 
 
-        balanceofA -= amountA; //Update the balance in the tokenA pool
-        balanceofB -= amountB; //Update the balance in the tokenB pool
+        balanceofA -= amountA;
+        balanceofB -= amountB;
 
-        tokenA.transfer(msg.sender, amountA); //I send the amount "amountA" to the owner
-        tokenB.transfer(msg.sender, amountB); //I send the amount "amountB" to the owner
+        tokenA.transfer(msg.sender, amountA);
+        tokenB.transfer(msg.sender, amountB);
 
-        emit LiquidityRemoved(amountA, amountB); //Generates "Decreased Liquidity" event
+        emit LiquidityRemoved(amountA, amountB);
     }
 
-    //Calculates the relative price of one token in relation to the other
+    /// @notice Calculates the relative price of one token in terms of the other
+    /// @param _token The address of the token to price
+    /// @return The price of the token in terms of the other token
     function getPrice(address _token) external view EnoughLiquidity returns (uint256) {
-        require(_token == address(tokenA) || _token == address(tokenB), "Invalid token address"); //Requires tokenA or tokenB to be used
+        require(_token == address(tokenA) || _token == address(tokenB), "Invalid token address"); 
 
-        if (_token == address(tokenA)) { //If it is tokenA, it returns the result of balanceB/balanceA
+        if (_token == address(tokenA)) {
             return (balanceofB * 1e18) / balanceofA;
-        } else { //If it is tokenB, it returns the result of balanceA/balanceB
+        } else { 
             return (balanceofA * 1e18) / balanceofB;
         }
     }
